8c93268281c39f9f4234af89952a3d1f3e95c678
Mehedi Hasan <developerhasan99@gmail.com>
2022-06-01 08:25:57 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      White: "hsl(0, 0%, 100%)",
+      Black: "hsl(0, 0%, 0%)",
+      DarkGray: "hsl(0, 0%, 55%)",
+      VeryDarkGray: "hsl(0, 0%, 41%)",
+      blue: "#5da5e7",
+    },
+    fontSize: {
+      xs: "14px",
+      sm: "15px",
+    },
+    fontFamily: {
+      alata: "Alata, sans-serif",
+      josefin: "Josefin Sans, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
